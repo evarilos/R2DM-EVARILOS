7626ca9b5ecbacdf58470f674c39124048ae294b
Filip Lemic <lemic@tkn.tu-berlin.de>
2015-05-11 14:24:12 +0200

    message_types

--- examples/deleteDatabase.py
+++ examples/deleteDatabase.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""deleteDatabase.py: Delete a database in the R2DM service."""
+
+__author__ = "Filip Lemic"
+__copyright__ = "Copyright 2015, EVARILOS Project"
+
+__version__ = "1.0.0"
+__maintainer__ = "Filip Lemic"
+__email__ = "lemic@tkn.tu-berlin.de"
+__status__ = "Development"
+
 import sys
 import urllib2
 import json
@@ -9,7 +22,7 @@ from generateURL import RequestWithMethod
 apiURL = 'http://localhost:5000/'
 
 # The ID of the database
-db_id = 'metadata'
+db_id = 'test_db'
 
 req = RequestWithMethod(apiURL + 'evarilos/raw_data/v1.0/database/' + db_id, 'DELETE', headers={"Content-Type": "application/json"})
 resp = urllib2.urlopen(req)
