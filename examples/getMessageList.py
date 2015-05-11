d51afdd6a35ab4743c0f0e41a79b12b9cd3eacfd
Filip Lemic <lemic@tkn.tu-berlin.de>
2015-05-11 14:36:09 +0200

    Examples

--- examples/getMessageList.py
+++ examples/getMessageList.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""getMessageList.py: Get a list of all messages in a collection in the R2DM service."""
+
+__author__ = "Filip Lemic"
+__copyright__ = "Copyright 2015, EVARILOS Project"
+
+__version__ = "1.0.0"
+__maintainer__ = "Filip Lemic"
+__email__ = "lemic@tkn.tu-berlin.de"
+__status__ = "Development"
+
 import sys
 import urllib2
 import json
@@ -8,13 +21,13 @@ import json
 apiURL = 'http://localhost:5000/'
 
 # The ID of the database
-db_id = 'testDatabase'
+db_id = 'test_db'
 
 # The ID of the collection in the database
-coll_id = 'testCollection_new'
+coll_id = 'test_coll'
 
-# The ID of teh data
-data_id = 'Loc1'
+# The ID of the data
+data_id = 'test_data'
 
 req = urllib2.Request(apiURL + 'evarilos/raw_data/v1.0/database/' + db_id  + '/collection/' + coll_id + '/message', headers={"Content-Type": "application/json"})
 resp = urllib2.urlopen(req)
@@ -23,11 +36,3 @@ messages = json.loads(resp.read())
 print "Location label:"
 for i in messages.keys():
 	print messages[i]['data_id']
-
-print "Metadata label:"
-for i in messages.keys():
-	print messages[i]['metadata_id']	
-
-print "ID given by the database:"
-for i in messages.keys():
-	print messages[i]['_id']	
