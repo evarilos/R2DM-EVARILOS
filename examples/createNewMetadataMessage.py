ec1274d758dd863565a4ec2b923ed5cb84dcc09d
Filip Lemic <lemic@tkn.tu-berlin.de>
2015-05-15 15:32:34 +0200

    Small changes.

--- examples/createNewMetadataMessage.py
+++ examples/createNewMetadataMessage.py
@@ -18,22 +18,22 @@ import json
 import time
 import sys
 
-apiURL = 'http://ebp.evarilos.eu:5000/'
+apiURL = 'http://localhost:5000/'
 db_id = 'metadata'
-coll_id = 'wifi_beacon_rssi_wilabt1'
+coll_id = 'test_coll'
 
 metadata = raw_metadata_pb2.Metadata() 
-metadata.data_id ='runtime'
-metadata.metadata_id = '13'
-metadata.testbed_id = 'w-iLab.t 1'
-metadata.licence = 'http://opendatacommons.org/licenses/odbl/summary/'
+metadata.data_id ='test_data'
+metadata.metadata_id = 'dummy'  # has to be the same as the metadata_id in the dataset that is described with this metadata message
+metadata.testbed_id = 'dummy'
+metadata.licence = 'dummy'
 metadata.timestamp_utc = int(time.mktime(datetime.utcnow().timetuple()))
-metadata.scenario.receiver_description = 'Receiver is a WiFi-enabled Linux device.'
-metadata.scenario.sender_description = 'Senders are all WiFi APs in the area of interest.'
-metadata.scenario.environment_description = 'w-iLab.t 1 testbed - office environment with ply-wooden walls.'
-metadata.scenario.experiment_description = 'Fingerprints are taken at uniformly distributed locations in the testbed environment.'
-metadata.scenario.type_of_raw_data = 'At different locations the RSSI fingerprints have been taken. Each fingerprint consists of 20 scans of the wireless environment and RSSI values from beacon packets from all visible APs are taken.'
-metadata.scenario.interference_description = 'This is an experiment without controlled interference. Measurements were done on a weekend afternoon.'
+metadata.scenario.receiver_description = 'dummy'
+metadata.scenario.sender_description = 'dummy'
+metadata.scenario.environment_description = 'dummy'
+metadata.scenario.experiment_description = 'dummy'
+metadata.scenario.type_of_raw_data = 'dummy'
+metadata.scenario.interference_description = 'dummy'
 obj = metadata.SerializeToString()
 
 req = urllib2.Request(apiURL + 'evarilos/raw_data/v1.0/database/' + db_id + '/collection/' + coll_id, headers={"Content-Type": "application/x-protobuf"}, data = obj)
